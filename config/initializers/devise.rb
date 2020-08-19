b8057a2ae578951c389c505b3f4eb723b88cdbda
Chase Florom <cflorom@trinitylife.org>
2020-08-18 20:17:25 -0500

    Install devise username

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -244,10 +244,10 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
-  # Configure the default scope given to Warden. By default it's the first
-  # devise role declared in your routes (usually :user).
+  # Configure the default scope given to Wardscen. By default it's the first
+  # devise role declared in your routes (usuasclly :user).
   # config.default_scope = :user
 
   # Set this configuration to false if you want /users/sign_out to sign out
